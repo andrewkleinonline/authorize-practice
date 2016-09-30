b3006d2b4fb5d6252c7f8073ea5b63e71aac1197
Jason Decastro <jasonrdecastro@gmail.com>
2016-09-30 16:26:18 -0400

    like 97% done

---
+++ frontend-react/src/components/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './nav_bar';
+import { Link } from 'react-router';
+
+export default (props) => {
+    return (
+      <div>
+        <NavBar title="Login" url="/" />
+        <div className='container'>
+          <Link to="/login">Login</Link>
+        </div>
+
+      </div>
+    )
+};
---
+++ frontend-react/src/components/nav_bar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {loggedIn: !!sessionStorage.getItem('jwt')}
+    this.logOut = this.logOut.bind(this)
+  }
+
+  logOut(event) {
+    sessionStorage.removeItem('jwt')
+
+  }
+
+  render() {
+    return (
+      <nav className='navbar navbar-inverse'>
+        <div className='navbar-header'>
+          <a className='navbar-brand' href={this.props.url}>{this.props.title}</a>
+          { "|" }
+          {sessionStorage.getItem('jwt') ? <a href="/login" onClick={this.logOut}>Log Out</a> : <Link to="/login">Log In</Link>}
+        </div>
+      </nav>
+    )
+  }
+}
